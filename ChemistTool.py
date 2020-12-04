1e2d043ba4d2cd3c763a0bf267d4708d5bc681ea
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2020-12-05 00:18:59 +0530

    new periodic

--- ChemistTool.py
+++ ChemistTool.py
@@ -1,1 +1,22 @@
 # program to display periodic table and more
+from tkinter import *
+import package.hovering as ho
+
+root = Tk()
+root.title('Periodic Table')
+
+# Centering the window
+root_h, root_w = 680, 1200
+s_w = root.winfo_screenwidth()
+s_h = root.winfo_screenheight()
+x_co = int((s_w / 2) - (root_w / 2))
+y_co = int((s_h / 2) - (root_h / 2))
+root.geometry("{}x{}+{}+{}".format(root_w, root_h, x_co, y_co))
+
+
+def elements_display(ele_name: str, at_no: str, ele_sym: str, at_mass: str, button_obj):
+    ho.CreateToolTip(button_obj, f'Element Symbol: {ele_sym}\nElement Name: {ele_name}\nAtomic Number: {at_no}'
+                                 f'\nAtomic Mass: {at_mass}')
+
+
+root.mainloop()

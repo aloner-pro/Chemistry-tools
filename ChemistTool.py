54ceeb0e41574042ffe83e30487619f8b7ae7e2b
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2020-12-05 00:28:35 +0530

    look good and testing

--- ChemistTool.py
+++ ChemistTool.py
@@ -14,9 +14,12 @@ y_co = int((s_h / 2) - (root_h / 2))
 root.geometry("{}x{}+{}+{}".format(root_w, root_h, x_co, y_co))
 
 
-def elements_display(ele_name: str, at_no: str, ele_sym: str, at_mass: str, button_obj):
-    ho.CreateToolTip(button_obj, f'Element Symbol: {ele_sym}\nElement Name: {ele_name}\nAtomic Number: {at_no}'
-                                 f'\nAtomic Mass: {at_mass}')
+def elements_display(ele_name: str, at_no: int, ele_sym: str, at_mass: float, button_obj):
+    ho.create_tool_tip(button_obj, f'Element Symbol: {ele_sym}\nElement Name: {ele_name}\nAtomic Number: {at_no}'
+                                   f'\nAtomic Mass: {at_mass}')
 
 
+bu_check = Button(root, text='Check')
+bu_check.pack()
+elements_display('Hydrogen', 1, 'H', 1.007, bu_check)
 root.mainloop()
--- package/hovering.py
+++ package/hovering.py
@@ -12,42 +12,42 @@ class ToolTip(object):
 
     def __init__(self, widget):
         self.widget = widget
-        self.tipwindow = None
+        self.tip_window = None
         self.id = None
         self.x = self.y = 0
 
     def showtip(self, text):
         """Display text in tooltip window"""
         self.text = text
-        if self.tipwindow or not self.text:
+        if self.tip_window or not self.text:
             return
         x, y, cx, cy = self.widget.bbox("insert")
         x = x + self.widget.winfo_rootx() + 57
         y = y + cy + self.widget.winfo_rooty() + 27
-        self.tipwindow = tw = Toplevel(self.widget)
+        self.tip_window = tw = Toplevel(self.widget)
         tw.wm_overrideredirect(1)
         tw.wm_geometry("+%d+%d" % (x, y))
         label = Label(tw, text=self.text, justify=LEFT,
                       background="#ffffe0", relief=SOLID, borderwidth=1,
-                      font=("tahoma", "8", "normal"))
+                      font=("Cascadia Code", "10", "normal"))
         label.pack(ipadx=1)
 
     def hidetip(self):
-        tw = self.tipwindow
-        self.tipwindow = None
+        tw = self.tip_window
+        self.tip_window = None
         if tw:
             tw.destroy()
 
 
-def CreateToolTip(widget, text):
+def create_tool_tip(widget, text):
     """Main function which is called in programs"""
-    toolTip = ToolTip(widget)
+    tool_tip = ToolTip(widget)
 
     def enter(event):
-        toolTip.showtip(text)
+        tool_tip.showtip(text)
 
     def leave(event):
-        toolTip.hidetip()
+        tool_tip.hidetip()
 
     widget.bind('<Enter>', enter)
     widget.bind('<Leave>', leave)

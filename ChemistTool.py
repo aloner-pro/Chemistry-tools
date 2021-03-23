8edcd2784f1277cdbb18b06eda0af634c9d8eb8d
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2021-03-23 22:22:58 +0530

    displayed first grp

--- ChemistTool.py
+++ ChemistTool.py
@@ -1,12 +1,19 @@
-# program to display periodic table and more
+# Program to display periodic table and more
+import csv
 from tkinter import *
 import package.hovering as ho
-from pandas import read_csv
 
-data = read_csv('queried.csv')
-value = [i for i in data['Group'] if str(i) != 'nan']
-value.sort()
-print([int(j) for j in value])
+with open('queried.csv', 'r') as d:
+    td = list(csv.reader(d))
+    data = []
+    for i in td[1:]:
+        if i[-3] != '':
+            data.append(i)
+    g1 = []
+    for k in range(18):
+        for j in data:
+            if int(j[-3]) == k:
+                g1.append(j)
 
 root = Tk()
 root.title('Periodic Table')
@@ -26,9 +33,9 @@ def elements_display(ele_name: str, at_no: int, ele_sym: str, at_mass: float, bu
 
 
 for i in range(7):
-    bu_check = Button(root, text='Check', height=5, width=10)
+    bu_check = Button(root, text=g1[i][0], height=5, width=10)
     bu_check.place(x=5, y=80 + i * 85)
-    elements_display('Hydrogen', 1, 'H', 1.007, bu_check)
+    elements_display(g1[i][1], g1[i][2], g1[i][0], g1[i][3], bu_check)
 
 exit_bu = Button(root, text='Exit', command=root.destroy, bg='red', fg='yellow')
 exit_bu.place(x=1100, y=600)

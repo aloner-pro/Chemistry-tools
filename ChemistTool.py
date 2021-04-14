ebcbeb6b7425d03cab27c9df7afad40de8933706
aloner-pro <69566363+aloner-pro@users.noreply.github.com>
2021-04-14 15:00:31 +0530

    some reformating

--- ChemistTool.py
+++ ChemistTool.py
@@ -38,7 +38,7 @@ def elements_display(ele_name: str, at_no: int, ele_sym: str, at_mass: float, bu
 def ele_7(ele_list, x_, color):
     for a in range(7):
         du = ele_list[a]
-        bu_check2 = Button(root, text=du[0], height=4, width=8, bg=color, relief='groove', font=('Times New Roman',10))
+        bu_check2 = Button(root, text=du[0], height=4, width=8, bg=color, relief='groove', font=('Times New Roman', 10))
         bu_check2.place(x=x_, y=40 + a * 70)
         elements_display(du[1], du[2], du[0], du[3], bu_check2)
 
@@ -46,7 +46,7 @@ def ele_7(ele_list, x_, color):
 def ele_6(ele_list, x_, color):
     for a in range(6):
         du = ele_list[a]
-        bu_check2 = Button(root, text=du[0], height=4, width=8, bg=color, relief='groove', font=('Times New Roman',10))
+        bu_check2 = Button(root, text=du[0], height=4, width=8, bg=color, relief='groove', font=('Times New Roman', 10))
         bu_check2.place(x=x_, y=110 + a * 70)
         elements_display(du[1], du[2], du[0], du[3], bu_check2)
 
@@ -54,7 +54,8 @@ def ele_6(ele_list, x_, color):
 def ele_4(ele_list, x_):
     for a in range(4):
         du = ele_list[a]
-        bu_check2 = Button(root, text=du[0], height=4, width=8, bg='#EEE3BC', relief='groove', font=('Times New Roman',10))
+        bu_check2 = Button(root, text=du[0], height=4, width=8, bg='#EEE3BC', relief='groove',
+                           font=('Times New Roman', 10))
         bu_check2.place(x=x_, y=250 + a * 70)
         elements_display(du[1], du[2], du[0], du[3], bu_check2)
 
@@ -62,7 +63,7 @@ def ele_4(ele_list, x_):
 def lan_act(ele, y_):
     for t in range(14):
         fe = ele[t]
-        bu_la = Button(root, text=fe[0], height=4, width=8, bg='#8287B4', relief='groove', font=('Times New Roman',10))
+        bu_la = Button(root, text=fe[0], height=4, width=8, bg='#8287B4', relief='groove', font=('Times New Roman', 10))
         bu_la.place(x=135 + t * 66, y=y_)
         elements_display(fe[1], fe[2], fe[0], fe[3], bu_la)
 
@@ -71,16 +72,17 @@ ele_7(g1[:7], 3, '#507F9B')
 ele_6(g1[7:13], 69, '#507F9B')
 
 for y in range(10):
-    ele_4(g1[13+(4*y):17+(4*y)], 135+(66*y))
+    ele_4(g1[13 + (4 * y):17 + (4 * y)], 135 + (66 * y))
 
 for z in range(5):
-    ele_6(g1[53+(6*z):59+(6*z)], 795+(66*z), '#89B6E4')
+    ele_6(g1[53 + (6 * z):59 + (6 * z)], 795 + (66 * z), '#89B6E4')
 
 ele_7(g1[83:90], 1125, '#89B6E4')
 lan_act(lan[:14], 545)
 lan_act(lan[14:], 615)
 
-exit_bu = Button(root, text='Exit', command=root.destroy, bg='red', fg='yellow', relief='groove', font=('Times New Roman',10))
+exit_bu = Button(root, text='Exit', command=root.destroy, bg='red', fg='yellow', relief='groove',
+                 font=('Times New Roman', 10))
 exit_bu.place(x=1100, y=600)
 ho.create_tool_tip(exit_bu, "Closes the window")
 root.mainloop()
--- README.md
+++ README.md
@@ -4,21 +4,9 @@
 + Main objective to lessen the lines of code.<br>
 + Upgrading the look and feel of the periodic table.
 
-## And basic chemical calculator
----
-**This contains the following features**
-+ Periodic Table
-+ Moles calculaor[In previous gen]
-+ **Concentration Terms like-**
-  1. Mass Percentage
-  2. Mole Fraction
-  3. Molality
-  4. Molarity
-
-Here's how the Periodic Table looks like after some mod-
+###How does it look
+Here's how the Periodic Table looks like after some working-
 ![Periodic](https://github.com/aloner-pro/Chemistry-tools/blob/master/new.png?raw=true)
-This also contains the actinoids and lanthanoids.
-To see them just click on those lanthanoids and actinoids buttons.
 
 >Written using basic python knowledge.
 >Plenty of scope to improve this.
